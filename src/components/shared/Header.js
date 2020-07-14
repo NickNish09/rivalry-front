1469258e324dc93beb859317bd77c7c32fc6f14f
nicknish09 <nnmarques97@gmail.com>
2020-07-14 17:35:59 -0300

    added responsive menu

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Headroom from "react-headroom";
 import logo from "../../assets/rivalry-logo.svg";
 
-import { Button } from "antd";
+import { Button, Divider } from "antd";
 import { Link } from "react-router-dom";
 
-import { SearchOutlined } from "@ant-design/icons";
+import { SearchOutlined, MenuOutlined } from "@ant-design/icons";
 import { isAuthenticated } from "../../services/auth";
 import { TOKEN_KEY } from "../../config/constants";
 
 const Header = () => {
+  const [openResponsiveMenu, setOpenResponsiveMenu] = useState(false);
   return (
     <Headroom>
       <div className={"menu-navbar"}>
@@ -42,8 +43,41 @@ const Header = () => {
               Login
             </Link>
           )}
+          <MenuOutlined
+            className={"menu-menu-icon"}
+            onClick={() => setOpenResponsiveMenu((prevValue) => !prevValue)}
+          />
         </div>
       </div>
+      {openResponsiveMenu ? (
+        <div className={"menu-responsive"}>
+          <div className={"menu-links-responsive"}>
+            <Link to="/">Home</Link>
+            <Link to="/trending">Trending</Link>
+            <Link to="/top">Top Rivalries</Link>
+            <Link to="/create">Create</Link>
+            <Divider className={"divider-responsive"} />
+            {isAuthenticated() ? (
+              <Button
+                onClick={() => {
+                  localStorage.removeItem(TOKEN_KEY);
+                  window.location.reload();
+                }}
+                type={"link"}
+                className={"menu-login-responsive"}
+              >
+                Leave
+              </Button>
+            ) : (
+              <Link to={"/login"} className={"menu-login"}>
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div />
+      )}
     </Headroom>
   );
 };
