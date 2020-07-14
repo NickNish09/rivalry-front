c6d903da1efd37865ede87eed407644f2460bc93
nicknish09 <nnmarques97@gmail.com>
2020-07-14 18:05:59 -0300

    fixed bug of staring without login

--- src/components/home/RivalryStarRow.js
+++ src/components/home/RivalryStarRow.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { StarFilled, StarOutlined } from "@ant-design/icons";
 import { usePrevious } from "../../helpers/usePrevious";
+import { openNotificationWithIcon } from "../../helpers/notifications";
+import { isAuthenticated } from "../../services/auth";
 
 export const RivalryStarRow = ({ rivalStarIndex, starRival, rival }) => {
   const [starCount, setStarCount] = useState(rival.stars);
@@ -24,16 +26,32 @@ export const RivalryStarRow = ({ rivalStarIndex, starRival, rival }) => {
         <StarFilled
           className={"rivalry-like"}
           onClick={() => {
-            starRival(rival.rival._id, false);
-            setStarCount((prevStarCount) => prevStarCount - 1); // is destaring, so gets one less
+            if (isAuthenticated()) {
+              starRival(rival.rival._id, false);
+              setStarCount((prevStarCount) => prevStarCount - 1); // is destaring, so gets one less
+            } else {
+              openNotificationWithIcon(
+                "error",
+                "Not logged in",
+                "You must loggin before giving a star."
+              );
+            }
           }}
         />
       ) : (
         <StarOutlined
           className={"rivalry-like"}
           onClick={() => {
-            starRival(rival.rival._id, true);
-            setStarCount((prevStarCount) => prevStarCount + 1); // is staring, increase value
+            if (isAuthenticated()) {
+              starRival(rival.rival._id, true);
+              setStarCount((prevStarCount) => prevStarCount + 1); // is staring, increase value
+            } else {
+              openNotificationWithIcon(
+                "error",
+                "Not logged in",
+                "You must loggin before giving a star."
+              );
+            }
           }}
         />
       )}
