7c5f30ce1887fd22a52055b6ea360584fad25146
nicknish09 <nnmarques97@gmail.com>
2020-07-15 20:20:41 -0300

    prepared to deploy

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { TOKEN_KEY } from "../config/constants";
 
 const dev_url = "http://localhost:3000/v1/";
-const production_url = "http://rivalry-api.herokuapp.com/v1/";
+const production_url =
+  "http://ec2-3-17-206-79.us-east-2.compute.amazonaws.com/v1/";
 
-export const baseURL = dev_url;
+export const baseURL = production_url;
 
 const api = axios.create({
   baseURL,
