ffaa23ae33ed07c318253c06b7d7b0c762715d5f
nicknish09 <nnmarques97@gmail.com>
2020-07-16 16:56:41 -0300

    adjusted default production url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const USER_KEY = "USER_KEY";
 export const TOKEN_KEY = "TOKEN_KEY";
-export const DEFAULT_PRODUCTION_URL = "https://rivalries.netlify.app/";
+export const DEFAULT_PRODUCTION_URL = "https://rivalries.netlify.app";
