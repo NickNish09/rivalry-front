5046f1c3c2805b9426f69a9df1d6109953e7c62f
nicknish09 <nnmarques97@gmail.com>
2020-07-15 23:38:37 -0300

    att api production url

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { TOKEN_KEY } from "../config/constants";
 
 const dev_url = "http://localhost:3000/v1/";
-const production_url =
-  "http://ec2-3-17-206-79.us-east-2.compute.amazonaws.com/v1/";
+const production_url = "https://rivalry-api.tk/v1/";
 
 export const baseURL = production_url;
 
